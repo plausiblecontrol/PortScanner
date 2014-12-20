ba70c9158b936490eb32f958f170a105509a363a
PlausibleControl <MrAndrewPedersen@gmail.com>
2014-12-19 21:52:29 -0600

    Improved error handling, and parameter functionality
    can handle single ports and port ranges as entries.

--- Program.cs
+++ Program.cs
@@ -10,102 +10,103 @@ using System.Net;
 namespace PartSconner {
   class Program {
     static void Main(string[] args) {
-      int firstP;
-      int lastP;
+      string hosts = "";
+      string ports = "";
       bool debug = false;
+      int time = 0;
+      int offset = 0;
+
+      try {//host and time
+        time = Math.Abs(Convert.ToInt32(args[0]));
+        offset = 1;
+        hosts = args[1];
+      } catch {
+        if(args.Length>0)
+          hosts = args[0];
+      }
+      try {//ports
+        ports = args[1+offset];
+      } catch { }
+
+      try {//debug
+        if (args[2 + offset] != null)
+          debug = true;
+      } catch { }
+
+      string[] portList = procPorts(ports);
+      string[] hostList = hosts.Split(',');
+
       if (args.Length == 0) {
         showHelp();
       } else {
         try {
-          if (args.Length > 3){
-            debug = true;
-            firstP = Convert.ToInt32(args[1]);
-          lastP = Convert.ToInt32(args[2]) + 1;
-          string[] hosts = args[0].Split(',');
-          foreach (string host in hosts) { 
-          Console.WriteLine("Scanning {0} from {1} to {2} for open ports...\nThis could take a few minutes.\n", host, firstP, lastP-1);
-          Parallel.For(firstP, lastP, i => {
-            Scan(i, host, debug);
-          });
-          }
+          int[] myPorts = portList.Select(int.Parse).ToArray();
+          if (time == 0) {
+            sconner(hostList, myPorts, debug);
           } else {
-            if (args.Length == 2) {
-              string[] hosts = args[0].Split(',');
-              int[] ports = args[1].Split(',').Select(int.Parse).ToArray();
-
-              foreach (string host in hosts) {
-                Console.WriteLine("Scanning {0} for open ports..\n", host);
-                Parallel.ForEach(ports, i => {
-                  Scan(i, host, debug);
-                });
-              }
-            } else {//args = 0 1 or 3
-              int[] ports = args[1].Split(',').Select(int.Parse).ToArray();
-              if(ports.Length>1){
-                if (args.Length == 3)
-                  debug = true;
-                string[] hosts = args[0].Split(',');
-                foreach (string host in hosts) {
-                  Console.WriteLine("Scanning {0} for open ports..\n", host);
-                  Parallel.ForEach(ports, i => {
-                    Scan(i, host, debug);
-                  });
-                }
-              } else {//ports = 1
-                try {
-                  lastP = Convert.ToInt32(args[2]) + 1;
-                  debug = false;
-                } catch {
-                  debug = true;
-                }
-                if (debug) {
-                  string[] hosts = args[0].Split(',');
-                  foreach (string host in hosts) {
-                    Scan(ports[0], host, debug);
-                  }
-                } else {
-                  string[] hosts = args[0].Split(',');
-                  firstP = Convert.ToInt32(args[1]);
-                  lastP = Convert.ToInt32(args[2]) + 1;
-                  foreach (string host in hosts) {
-                    Console.WriteLine("Scanning {0} from {1} to {2} for open ports...\nThis could take a few minutes.\n", host, firstP, lastP - 1);
-                    Parallel.For(firstP, lastP, i => {
-                      Scan(i, host, debug);
-                    });
-                  }
-                }
-                
-                
-              }
+            string[] tArgs = args.Skip(1).ToArray();
+            while (true) {
+              sconner(hostList, myPorts, debug);
+              System.Threading.Thread.Sleep(time * 1000);
             }
           }
         } catch {
+          Console.WriteLine("\nBad entry, see usage:\n");
           showHelp();
-        }
+        } 
       }
       Console.WriteLine("\nFinished.");
     }
 
+    public static string[] procPorts(string ports) {
+      List<string> p = new List<string>();
+      string[] procSplit = ports.Split(',');
+      foreach (string ss in procSplit) {
+        string[] dashes = ss.Split('-');
+        if (dashes.Length == 2) {
+          for (int i = Convert.ToInt32(dashes[0]); i <= Convert.ToInt32(dashes[1]); i++) {
+            p.Add(i.ToString());
+          }
+        } else {
+          p.Add(dashes[0]);
+        }
+      }
+      return p.ToArray();
+    }
+
+    public static void sconner(string[] hosts, int[] ports, bool debug) {
+      try {
+        foreach (string host in hosts) {
+          if (debug) {
+            Console.WriteLine("\nScanning {0}'s ports..", host);
+          } else {
+            Console.WriteLine("\nScanning {0} for open ports only..", host);
+          }          
+          Parallel.ForEach(ports, i => {
+            Scan(i, host, debug);
+          });
+        }
+      } catch {
+        Console.WriteLine("There was bad data in your parameters.");
+      }
+    }
+
     public static void showHelp() {
-      Console.WriteLine("PartSconner.exe [host/ip,host2/ip2,..] [starting port] [ending port] (showall?)");
-      Console.WriteLine("PartSconner.exe [host/ip,host2/ip2,..] [port,port2,..] (showall?)");
+      Console.WriteLine("PartSconner.exe (loop time in s) [host/ip,host2/ip2,..] [port,port2,port3-port8,port9..] (showall?)");
     }
 
     public static void Scan(int port, string txtIPaddress, bool allData) {
       TcpClient partS = new TcpClient();
-      partS.SendTimeout = 10;
-      partS.ReceiveTimeout = 10;
-      try {
-        partS.Connect(txtIPaddress, port);
-        Console.WriteLine("Port " + port + " open!!!");
-      } catch (Exception e){
-        if (allData) {
-          if (e.Message.Substring(0, 1) == "N") {
-            Console.WriteLine("Port " + port + " was blocked.");
-          } else {
-            Console.WriteLine("Port " + port + " is closed.");
-          }
-        }
+      partS.SendTimeout = 2000;
+      partS.ReceiveTimeout = 2000;
+      IAsyncResult result = partS.BeginConnect(txtIPaddress, port, null, null);
+      bool success = result.AsyncWaitHandle.WaitOne(2000, true);
+      if (success && partS.Connected) {
+        Console.WriteLine(txtIPaddress + ":" + port + " open!!!");
+      } else if (success && allData && !partS.Connected) {
+        Console.WriteLine(txtIPaddress + ":" + port + " blocked.");
+      } else if (!success && allData) {
+        Console.WriteLine(txtIPaddress + ":" + port + " timed out.");
       }
       partS.Close();
     }
